4813da72529ab3af5642b0e5c353d437482232bb
popcornmix <popcornmix@gmail.com>
2016-01-02 20:19:23 +0000

    [debug] Log when playback start takes too long

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -3761,6 +3761,7 @@ bool CApplication::OnMessage(CGUIMessage& message)
 
   case GUI_MSG_PLAYBACK_STARTED:
     {
+      int64_t Start = CurrentHostCounter();
 #ifdef TARGET_DARWIN_IOS
       // @TODO move this away to platform code
       CDarwinUtils::SetScheduling(m_appPlayer.IsPlayingVideo());
@@ -3806,6 +3807,10 @@ bool CApplication::OnMessage(CGUIMessage& message)
         if (dialog && !dialog->IsDialogRunning())
           dialog->WaitOnEvent(m_playerEvent);
       }
+
+      float duration = (CurrentHostCounter()-Start) * 1e-9;
+      if (duration > 0.1f)
+        CLog::LogF(LOGWARNING, "Suspiciously long time to handle GUI_MSG_PLAYBACK_STARTED (%.2fs)", duration);
 
       return true;
     }
