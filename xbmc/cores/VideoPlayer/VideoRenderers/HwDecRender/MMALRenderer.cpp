fd42efb18ee36809ddc9c2fff3aaf5a51f38d3d9
popcornmix <popcornmix@gmail.com>
2016-07-24 20:59:06 +0100

    mmalrender: Disable deinterlace when no interlaced frames seen

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/MMALRenderer.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/MMALRenderer.cpp
@@ -596,6 +596,7 @@ CMMALRenderer::CMMALRenderer() : CThread("MMALRenderer"), m_processThread(this, 
   m_cachedSourceRect.SetRect(0, 0, 0, 0);
   m_cachedDestRect.SetRect(0, 0, 0, 0);
   m_isPi1 = g_RBP.RaspberryPiVersion() == 1;
+  m_interlace_history = 0;
 
   m_queue_process = mmal_queue_create();
   m_processThread.Create();
@@ -743,6 +744,8 @@ void CMMALRenderer::Run()
         }
         bool interlace = (omvb->mmal_buffer->flags & MMAL_BUFFER_HEADER_VIDEO_FLAG_INTERLACED) ? true:false;
 
+        m_interlace_history = (m_interlace_history << 1) | (interlace ? 1 : 0);
+
         // advanced deinterlace requires 3 frames of context so disable when showing stills
         if (omvb->m_stills)
         {
@@ -761,7 +764,9 @@ void CMMALRenderer::Run()
             interlace_method = VS_INTERLACEMETHOD_MMAL_BOB_HALF;
         }
 
-        if (interlace_method == VS_INTERLACEMETHOD_NONE && !yuv16)
+        bool was_enabled = m_deint_input != nullptr;
+
+        if ((interlace_method == VS_INTERLACEMETHOD_NONE || !m_interlace_history) && !yuv16)
         {
           if (m_deint_input)
             DestroyDeinterlace();
@@ -788,6 +793,8 @@ void CMMALRenderer::Run()
           omvb->SetVideoDeintMethod("bob(x1)");
         else
           omvb->SetVideoDeintMethod("none");
+
+        CLog::Log(LOGDEBUG, "%s::%s im:%d in:%d(%llx) flags:%x enabled:%d->%d", CLASSNAME, __func__, interlace_method, interlace, m_interlace_history, omvb->mmal_buffer->flags, was_enabled, m_deint_input != nullptr);
 
         if (m_deint_input)
         {
