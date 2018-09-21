4f5dec10f1c863051a6ea01b0f7f5886187243c0
popcornmix <popcornmix@gmail.com>
2018-09-21 18:42:34 +0100

    Revert "Merge pull request #14459 from ace20022/bd_revert_menu_still"
    This reverts commit 6deac11f73f1003d9d0a056117b6a90ecde2bdd0, reversing
    changes made to a2133e432e7573da4ba6d99322f01fc9690c381d.

--- xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -429,8 +429,14 @@ void CDVDInputStreamBluray::ProcessEvent() {
 
     pid = m_event.param;
 
-    if (pid == 0)
-      m_player->OnDiscNavResult(static_cast<void*>(&pid), BD_EVENT_STILL);
+    if (pid == 1)
+    {
+      m_hold = HOLD_PAUSE;
+    }
+    else
+      m_hold = HOLD_HELD; //HOLD_NONE
+
+    m_player->OnDiscNavResult(static_cast<void*>(&pid), BD_EVENT_STILL);
     break;
 
     /* playback position */
@@ -452,6 +458,11 @@ void CDVDInputStreamBluray::ProcessEvent() {
         m_event.param);
     /* when a title ends, playlist WILL eventually change */
     FreeTitleInfo();
+    if (m_event.param == BLURAY_TITLE_TOP_MENU)
+    {
+      m_title = nullptr;
+      m_menu = false;
+    }
     break;
 
   case BD_EVENT_TITLE:
@@ -472,6 +483,7 @@ void CDVDInputStreamBluray::ProcessEvent() {
     else
       m_title = nullptr;
 
+    m_menu = false;
     break;
   }
   case BD_EVENT_PLAYLIST:
@@ -576,7 +588,8 @@ int CDVDInputStreamBluray::Read(uint8_t* buf, int buf_size)
   {
     do {
 
-      if (m_hold == HOLD_HELD)
+      if (m_hold == HOLD_PAUSE ||
+          m_hold == HOLD_HELD)
          return 0;
 
       if(  m_hold == HOLD_ERROR
@@ -728,8 +741,11 @@ void CDVDInputStreamBluray::OverlayFlush(int64_t pts)
   }
 
   m_player->OnDiscNavResult(static_cast<void*>(group), BD_EVENT_MENU_OVERLAY);
+  if (group->m_overlays.empty())
+    m_menu = false;
+  else
+    m_menu = true;
   group->Release();
-  m_menu = true;
 #endif
 }
 
@@ -1005,7 +1021,7 @@ CDVDInputStream::ENextStream CDVDInputStreamBluray::NextStream()
   while(bd_get_event(m_bd, &m_event))
     ProcessEvent();
 
-  if(m_hold == HOLD_STILL)
+  if(m_hold == HOLD_STILL || m_hold == HOLD_PAUSE)
     return NEXTSTREAM_RETRY;
 
   m_hold = HOLD_DATA;
@@ -1104,7 +1120,8 @@ void CDVDInputStreamBluray::SkipStill()
   if(m_bd == nullptr || !m_navmode)
     return;
 
-  if ( m_hold == HOLD_STILL)
+  if ( m_hold == HOLD_STILL
+    || m_hold == HOLD_PAUSE)
   {
     m_hold = HOLD_HELD;
     bd_read_skip_still(m_bd);
