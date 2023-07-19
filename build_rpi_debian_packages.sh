4bca08508a4c2e4357cabf0316f4286c61635157
Dom Cobley <popcornmix@gmail.com>
2023-07-19 20:04:14 +0100

    build_rpi_debian_packages.sh: Patch game.libretro use default tar files

--- build_rpi_debian_packages.sh
+++ build_rpi_debian_packages.sh
@@ -193,6 +193,10 @@ function compileAddons {
 		if [[ $D == "inputstream.adaptive" ]]; then
 			sed -i "s/\-DBENTO4_URL=bento4\.tar\.gz//" debian/rules
 		fi
+		if [[ $D == "game.libretro" ]]; then
+			sed -i "s/\-DLIBRETROCOMMON_URL=libretro-common\.tar\.gz//" debian/rules
+			sed -i "s/\-DRCHEEVOS_URL=rcheevos.tar.gz//" debian/rules
+		fi
 		if [[ $D == "inputstream.ffmpegdirect" ]]; then
 			sed -i "s/kodi-ffmpeg-dev,//" debian/control
 			curl -L https://github.com/xbmc/inputstream.ffmpegdirect/commit/e6541a98b64631836336de17e57efa8e8e0b8425.diff | patch -Rp1
