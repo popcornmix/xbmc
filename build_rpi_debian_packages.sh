5db98fb7e81b1722b2c4e3eedb13c1216a116fa0
Dom Cobley <popcornmix@gmail.com>
2025-07-30 15:36:38 +0100

    build_rpi_debian_packages: Update ffmpeg_direct patch to ffmpeg 7

--- build_rpi_debian_packages.sh
+++ build_rpi_debian_packages.sh
@@ -203,7 +203,7 @@ function compileAddons {
 		fi
 		if [[ $D == "inputstream.ffmpegdirect" ]]; then
 			sed -i "s/kodi-ffmpeg-dev,//" debian/control
-			curl -L https://github.com/xbmc/inputstream.ffmpegdirect/commit/e6541a98b64631836336de17e57efa8e8e0b8425.diff | patch -Rp1
+			curl -L https://github.com/xbmc/inputstream.ffmpegdirect/pull/304/commits/3b2c42c56972f894f0c7aac443908992bf22ea1e.diff | patch -p1
 		fi
 
 		dpkg-buildpackage $DEBUILD_OPTS -us -uc -b |& tee -a build_addons.log
