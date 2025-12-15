aff0909697866bb8677b86ca3cde74056c980ab3
Jonas Karlman <jonas@kwiboo.se>
2025-12-15 18:17:24 +0000

    CVideoBufferDRMPRIMEFFmpeg: Map HW AVFrame to AV_PIX_FMT_DRM_PRIME

--- xbmc/cores/VideoPlayer/Buffers/VideoBufferDRMPRIME.cpp
+++ xbmc/cores/VideoPlayer/Buffers/VideoBufferDRMPRIME.cpp
@@ -116,10 +116,48 @@ void CVideoBufferDRMPRIMEFFmpeg::Unref()
   av_frame_unref(m_pFrame);
 }
 
+AVDRMFrameDescriptor* CVideoBufferDRMPRIMEFFmpeg::GetDescriptor() const
+{
+  if (m_pMapFrame)
+    return reinterpret_cast<AVDRMFrameDescriptor*>(m_pMapFrame->data[0]);
+
+  return reinterpret_cast<AVDRMFrameDescriptor*>(m_pFrame->data[0]);
+}
+
+bool CVideoBufferDRMPRIMEFFmpeg::RequiresMapping() const
+{
+  return m_pFrame->format != AV_PIX_FMT_DRM_PRIME && m_pFrame->hw_frames_ctx;
+}
+
 bool CVideoBufferDRMPRIMEFFmpeg::IsValid() const
 {
+  if (RequiresMapping())
+    return true;
+
   AVDRMFrameDescriptor* descriptor = GetDescriptor();
   return descriptor && descriptor->nb_layers;
+}
+
+bool CVideoBufferDRMPRIMEFFmpeg::AcquireDescriptor()
+{
+  if (RequiresMapping())
+  {
+    m_pMapFrame = av_frame_alloc();
+    m_pMapFrame->format = AV_PIX_FMT_DRM_PRIME;
+
+    if (av_hwframe_map(m_pMapFrame, m_pFrame, AV_HWFRAME_MAP_READ))
+    {
+      av_frame_free(&m_pMapFrame);
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void CVideoBufferDRMPRIMEFFmpeg::ReleaseDescriptor()
+{
+  av_frame_free(&m_pMapFrame);
 }
 
 CVideoBufferPoolDRMPRIMEFFmpeg::~CVideoBufferPoolDRMPRIMEFFmpeg()
